8b8be9d740642996c264956fcfbd36314fa7ebc2
Rahwa Gebreyesus <rahwa3113@gmail.com>
2024-10-09 12:05:58 +0300

    javascript

---
+++ exersice/index.js
@@ -0,0 +1,11 @@
+const add=(a,b)=>a+b;
+console.log(`${add(2,3)}`);
+
+const multiply=(a,b)=>a*b;
+console.log(`${multiply(2,3)}`);
+
+
+
+const greet=(name)=>'Hello' + name;
+
+console.log(`${greet(' Rahwa')}`);
